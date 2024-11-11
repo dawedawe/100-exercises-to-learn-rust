cb7c7e45ab9f22bccde051d10e6b105f61c751f3
dawe <dawedawe@posteo.de>
2024-11-11 23:23:44 +0100

    exercises 06

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,14 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut v: Vec<u32> = vec![0, 1];
+    let n = n as usize;
+
+    for i in 2..=n {
+        let next = v[i - 1] + v[i - 2];
+        v.push(next);
+    }
+    v[n]
 }
 
 #[cfg(test)]
